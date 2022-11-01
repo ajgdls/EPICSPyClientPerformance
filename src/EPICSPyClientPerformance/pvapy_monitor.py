237da4b047319242aaed1bb26ec08a5a6ef90c4d
Tom Cobb <tom.cobb@diamond.ac.uk>
2022-11-01 10:47:59 +0000

    Tidy up monitor clients

--- src/EPICSPyClientPerformance/pvapy_monitor.py
+++ src/EPICSPyClientPerformance/pvapy_monitor.py
@@ -1,45 +1,31 @@
-
-import logging
 from functools import partial
+
 import pvaccess
-import time
 
 from EPICSPyClientPerformance.monitor_client import MonitorClient
-
-
-pvapy_client = None
-
-
-# Create the monitor callback
-def pvapy_callback(pv_name, value):
-    logging.debug("PV [{}] Value: {}  Timestamp: {}  Severity: {}".format(
-        pv_name,
-        value['value'],
-        value['timeStamp']['secondsPastEpoch'] + float(value['timeStamp']['nanoseconds'])/1000000000,
-        value['alarm']['severity']
-        ))
-    pvapy_client.add_sample(
-        pv_name,
-        value['value'],
-        value['timeStamp']['secondsPastEpoch'] + float(value['timeStamp']['nanoseconds'])/1000000000,
-        value['alarm']['severity']
-        )
 
 
 class PvapyMonitor(MonitorClient):
     def __init__(self):
         super(PvapyMonitor, self).__init__()
         self._subscriptions = []
-        global pvapy_client
-        pvapy_client = self
 
     def create_monitors(self):
         for pv_name in self._pv_names:
-            cb = partial(pvapy_callback, pv_name)
+            cb = partial(self.callback, pv_name)
             c = pvaccess.Channel(pv_name)
             c.subscribe(pv_name, cb)
-            c.startMonitor('field(value,alarm,timeStamp)')
+            c.startMonitor("field(value,alarm,timeStamp)")
             self._subscriptions.append(c)
+
+    def callback(self, pv_name, value):
+        self.add_sample(
+            pv_name,
+            value["value"],
+            value["timeStamp"]["secondsPastEpoch"]
+            + float(value["timeStamp"]["nanoseconds"]) / 1000000000,
+            value["alarm"]["severity"],
+        )
 
     def close(self):
         for sub, pv_name in zip(self._subscriptions, self._pv_names):
--- src/EPICSPyClientPerformance/pyepics_monitor.py
+++ src/EPICSPyClientPerformance/pyepics_monitor.py
@@ -1,24 +1,16 @@
-from epics import camonitor
+from epics import camonitor, camonitor_clear
 
 from EPICSPyClientPerformance.monitor_client import MonitorClient
 
 
-client = None
-
-# Create the monitor callback
-def pyepics_callback(pvname, value, char_value, **kw):
-    client.add_sample(pvname, value, kw['timestamp'], kw['severity'])
-
-
 class PyEPICSMonitor(MonitorClient):
-    def __init__(self):
-        super(PyEPICSMonitor, self).__init__()
-        self._subscriptions = []
-        global client
-        client = self
-
     def create_monitors(self):
         for pv_name in self._pv_names:
-            sub = camonitor(pv_name, writer=None, callback=pyepics_callback)
-            self._subscriptions.append(sub)
+            camonitor(pv_name, writer=None, callback=self.callback)
 
+    def callback(self, pvname, value, char_value, **kw):
+        self.add_sample(pvname, value, kw["timestamp"], kw["severity"])
+
+    def close(self):
+        for pv_name in self._pv_names:
+            camonitor_clear(pv_name)
