a6e1a67ed184428437f3510858b39a492d309637
Alan Greer <alan.greer@diamond.ac.uk>
2022-11-04 16:22:11 +0000

    Recommendation for altering the connection method.

--- src/EPICSPyClientPerformance/pyepics_monitor.py
+++ src/EPICSPyClientPerformance/pyepics_monitor.py
@@ -1,12 +1,11 @@
-from epics import camonitor, camonitor_clear
+from epics import camonitor_clear, get_pv
 
 from EPICSPyClientPerformance.monitor_client import MonitorClient
 
 
 class PyEPICSMonitor(MonitorClient):
     def create_monitors(self):
-        for pv_name in self._pv_names:
-            camonitor(pv_name, writer=None, callback=self.callback)
+        self.pvs = [get_pv(name, callback=self.callback) for name in self._pv_names]
 
     def callback(self, pvname, value, char_value, **kw):
         self.add_sample(pvname, value, kw["timestamp"], kw["severity"])
